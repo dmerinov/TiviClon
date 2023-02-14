d7723337e902cbc80dd1bfec66590c7ce02a20c2
David Merino Vecino <merinovecinod@gmail.com>
2023-02-14 19:02:41 +0100

    TA-1: Added Login Presenter

---
+++ app/src/main/java/com/example/tiviclon/presenters/LoginPresenter.kt
@@ -0,0 +1,12 @@
+package com.example.tiviclon.presenters
+
+class LoginPresenter (private val view: LoginView) {
+
+    fun initialize(){
+        view.setUpUI()
+    }
+}
+
+interface LoginView{
+    fun setUpUI()
+}
