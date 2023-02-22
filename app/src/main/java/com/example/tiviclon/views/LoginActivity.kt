812b19692a60825337f54b5e5cc97a554909c8e9
David Merino Vecino <merinovecinod@gmail.com>
2023-02-22 16:31:46 +0100

    TA-2: Removed architecture from project

--- app/src/main/java/com/example/tiviclon/views/LoginActivity.kt
+++ app/src/main/java/com/example/tiviclon/views/LoginActivity.kt
@@ -5,23 +5,20 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiviclon.R
 import com.example.tiviclon.databinding.ActivityLoginBinding
-import com.example.tiviclon.presenters.LoginPresenter
-import com.example.tiviclon.presenters.LoginView
 
-class LoginActivity : AppCompatActivity(), LoginView {
+class LoginActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityLoginBinding
-    private val presenter = LoginPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        presenter.initialize()
+        setUpUI()
     }
 
-    override fun setUpUI() {
+    private fun setUpUI() {
         //get attributes from xml using binding
         setUpListeners()
     }
@@ -29,16 +26,28 @@ class LoginActivity : AppCompatActivity(), LoginView {
     private fun setUpListeners() {
         with(binding) {
             btLogin.setOnClickListener {
-                presenter.checkCredentials(etUsername.text.toString(), etPassword.text.toString())
+                checkCredentials(etUsername.text.toString(), etPassword.text.toString())
             }
         }
     }
 
-    override fun navigateToHomeActivity() {
+    private fun checkCredentials(username: String, password: String) {
+        if (username.length > 6 && username.isNotBlank()) {
+            if (password.length > 6 && password.isNotBlank()) {
+                navigateToHomeActivity()
+            } else {
+                notifyInvalidCredentials()
+            }
+        } else {
+            notifyInvalidCredentials()
+        }
+    }
+
+    private fun navigateToHomeActivity() {
         Toast.makeText(this, getString(R.string.valid_user_msg), Toast.LENGTH_SHORT).show()
     }
 
-    override fun notifyInvalidCredentials() {
+    private fun notifyInvalidCredentials() {
         Toast.makeText(this, getString(R.string.invalid_user_msg), Toast.LENGTH_SHORT).show()
     }
 }
