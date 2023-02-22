133fc25e223ecfcdf98c54589d0d319396b0e1d6
David Merino Vecino <merinovecinod@gmail.com>
2023-02-22 16:36:08 +0100

    TA-2: cleaned code

--- app/src/main/java/com/example/tiviclon/views/LoginActivity.kt
+++ app/src/main/java/com/example/tiviclon/views/LoginActivity.kt
@@ -1,12 +1,10 @@
 package com.example.tiviclon.views
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.tiviclon.R
 import com.example.tiviclon.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity(){
+class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
@@ -26,28 +24,20 @@ class LoginActivity : AppCompatActivity(){
     private fun setUpListeners() {
         with(binding) {
             btLogin.setOnClickListener {
-                checkCredentials(etUsername.text.toString(), etPassword.text.toString())
+                // TODO - implement listener
             }
         }
     }
 
     private fun checkCredentials(username: String, password: String) {
-        if (username.length > 6 && username.isNotBlank()) {
-            if (password.length > 6 && password.isNotBlank()) {
-                navigateToHomeActivity()
-            } else {
-                notifyInvalidCredentials()
-            }
-        } else {
-            notifyInvalidCredentials()
-        }
+        //TODO - implement logic
     }
 
     private fun navigateToHomeActivity() {
-        Toast.makeText(this, getString(R.string.valid_user_msg), Toast.LENGTH_SHORT).show()
+        //TODO - simulate navigation
     }
 
     private fun notifyInvalidCredentials() {
-        Toast.makeText(this, getString(R.string.invalid_user_msg), Toast.LENGTH_SHORT).show()
+        //TODO - simulate not valid toast
     }
 }
