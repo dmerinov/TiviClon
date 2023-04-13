71361bc55b57af9589c2b7492a402da68bbcdd34
David Merino <merinovecinod@gmail.com>
2023-04-13 23:28:18 +0200

    TA-5: Appbar working

--- app/src/main/java/com/example/tiviclon/home/HomeActivity.kt
+++ app/src/main/java/com/example/tiviclon/home/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.tiviclon.R
@@ -45,7 +47,26 @@ class HomeActivity : AppCompatActivity(), HomeView {
         with(binding){
             appBar.title = "Buenas, ${user.name}"
             appBar.setTitleTextColor(Color.WHITE)
+            //appBar will not work without this
+            setSupportActionBar(appBar)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.actionExit) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.top_bar_menu, menu)
+        return true
     }
 
     override fun setUpListeners() {
